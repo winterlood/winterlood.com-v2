84786d524fe6c27b47c7c131a9ce4b498f3899cb
winterlood <king199777@gmail.com>
2024-11-15 16:41:03 +0900

    Create 한입커뮤니티소개.mdx

--- publish/util/get-content-notion-id.ts
+++ publish/util/get-content-notion-id.ts
@@ -1,17 +1,16 @@
 export function getContentNotionID() {
-  const script = process.env.npm_lifecycle_script;
-  if (!script) {
-    throw new Error("스크립트를 불러올 수 없습니다.");
+  const contentType = process.argv[4];
+  const contentId = process.argv[5];
+
+  console.log({ contentType, contentId });
+
+  if (
+    contentType.toLocaleLowerCase() !== "post" &&
+    contentType.toLocaleLowerCase() !== "qna"
+  ) {
+    throw new Error("스크립트 사용이 잘못되었습니다. TYPE 명시 없음");
   }
 
-  const splitedScripts = script.replace(" --env-file .env", "").split(" ");
-  const splitedScriptLength = splitedScripts?.length;
-  if (splitedScriptLength !== 4 && splitedScriptLength !== 5) {
-    throw new Error("ID혹은 TYPE이 명시되지 않았거나, 스크립트 사용이 잘못되었습니다");
-  }
-
-  const contentType = splitedScriptLength === 4 ? splitedScripts[2] : splitedScripts[3];
-  const contentId = splitedScriptLength === 4 ? splitedScripts[3] : splitedScripts[4];
   return {
     contentId: contentId.replaceAll('"', ""),
     contentType: contentType.replaceAll('"', ""),
